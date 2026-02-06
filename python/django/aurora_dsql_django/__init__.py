1fc6a28c57a309587399849a29782ff9fb075df9
Victor Tsang <vitsangp@amazon.com>
2026-02-06 11:05:33 -0800

    initial commit for sequence and identity support

--- python/django/aurora_dsql_django/__init__.py
+++ python/django/aurora_dsql_django/__init__.py
@@ -2,8 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from importlib.metadata import PackageNotFoundError, version
+from .fields import SequenceAutoField
 
 try:
     __version__ = version("aurora_dsql_django")
 except PackageNotFoundError:
     __version__ = "unknown"
+
+__all__ = ["SequenceAutoField"]
